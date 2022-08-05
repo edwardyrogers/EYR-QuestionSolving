8317a154cffe844f6034e97001a96c1866c2b0f7
Edward Y. Rogers <edwardyrogers@EYRs-M1.local>
2022-08-05 10:57:54 +0800

    update

--- LinkedList/Golang/src/AddTwoNumbers/main.go
+++ LinkedList/Golang/src/AddTwoNumbers/main.go
@@ -9,9 +9,9 @@ func main() {
 	// https://leetcode.com/problems/add-two-numbers/
 
 	listOne := data_structures.LinkedList{}
-	listOne.Insert(3)
-	listOne.Insert(2)
-	listOne.Insert(1)
+	listOne.Insert(7)
+	listOne.Insert(8)
+	listOne.Insert(9)
 
 	listTwo := data_structures.LinkedList{}
 	listTwo.Insert(6)
@@ -28,6 +28,45 @@ func main() {
 }
 
 func addTwoNumbers(l1 *data_structures.LinkNode, l2 *data_structures.LinkNode) *data_structures.LinkNode {
+	var carry = 0
+	var result = &data_structures.LinkNode{}
+	var pointer = result
 
-	return nil
+	for l1 != nil || l2 != nil || carry > 0 {
+
+		var first_num int
+		if l1 != nil {
+			first_num = int(l1.Val.(int))
+		} else {
+			first_num = 0
+		}
+
+		var second_num int
+		if l2 != nil {
+			second_num = int(l2.Val.(int))
+		} else {
+			second_num = 0
+		}
+
+		var sum = first_num + second_num + carry
+		var num = sum % 10
+		carry = sum / 10
+
+		pointer.Next = &data_structures.LinkNode{Val: num, Next: nil}
+		pointer = pointer.Next
+
+		if l1 != nil {
+			l1 = l1.Next
+		} else {
+			l1 = nil
+		}
+
+		if l2 != nil {
+			l2 = l2.Next
+		} else {
+			l2 = nil
+		}
+	}
+
+	return result.Next
 }

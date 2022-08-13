5c7770963b29e303cc0b70c52261683667ff35f7
Edward Y. Rogers <edwardyrogers@EYRs-M1.home>
2022-08-13 10:47:31 +0800

    update

--- LinkedList/Golang/src/MergeKSortedLists/main.go
+++ LinkedList/Golang/src/MergeKSortedLists/main.go
@@ -7,25 +7,49 @@ import (
 )
 
 func main() {
-	// https://leetcode.com/problems/merge-two-sorted-lists/
+	// https://leetcode.com/problems/merge-k-sorted-lists/
 
 	listOne := data_structures.LinkedList{}
-	listOne.Insert(1)
-	listOne.Insert(3)
+	listOne.Insert(0)
+	listOne.Insert(2)
 	listOne.Insert(5)
 
 	listTwo := data_structures.LinkedList{}
-	listTwo.Insert(2)
-	listTwo.Insert(4)
-	listTwo.Insert(6)
+	listTwo.Insert(0)
 
-	iter := mergeTwoLists(listOne.Head, listTwo.Head)
+	listThree := data_structures.LinkedList{}
+	listThree.Insert(-1)
+
+	lists := []*data_structures.LinkNode{listOne.Head, listTwo.Head, listThree.Head}
+
+	iter := mergeKLists(lists)
 
 	for iter != nil {
 		fmt.Println(iter.Val)
 
 		iter = iter.Next
 	}
+}
+
+func mergeKLists(lists []*data_structures.LinkNode) *data_structures.LinkNode {
+
+	var result = &data_structures.LinkNode{Val: 0, Next: nil}
+	var tempResult = result
+
+	for idx := 0; idx <= len(lists)-1; idx += 2 {
+
+		var listA *data_structures.LinkNode = lists[idx]
+		var listB *data_structures.LinkNode = nil
+
+		if ((idx + 1) % len(lists)) != 0 {
+			listB = lists[idx+1]
+		}
+
+		mergedLists := mergeTwoLists(listA, listB)
+		tempResult.Next = mergeTwoLists(tempResult.Next, mergedLists)
+	}
+
+	return result.Next
 }
 
 func mergeTwoLists(list1 *data_structures.LinkNode, list2 *data_structures.LinkNode) *data_structures.LinkNode {
